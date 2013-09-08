453e4eb3e00d19dc36327c065700920342dca6a5
Russell Folk <rfolk@russellfolk.com>
2013-09-07 22:14:19 -0600

    Added whitespace for readability.

--- Assignment1/sumAll.pl
+++ Assignment1/sumAll.pl
@@ -2,6 +2,7 @@
 % CS471: Assignment 1
 
 % Sum all the numbers in a list and return the value N.
+
 sumAll( [] , 0 ).             % if no numbers, then N is 0.
 sumAll( [Head | Tail] , N ):- % if numbers, count recursively.
     sumAll( Tail , NC ),      % count recursively after the Head
