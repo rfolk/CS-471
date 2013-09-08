fc617d6cffc9a3ade053ea25308150db9999e01e
Russell Folk <rfolk@russellfolk.com>
2013-09-07 19:34:17 -0600

    Added spacing and comments for readability.

--- Assignment1/countZeros.pl
+++ Assignment1/countZeros.pl
@@ -4,10 +4,10 @@
 % Count the number of zeros in a given list and return the value N based on the
 % number counted, NC, using the recursive solution.
 
-countZeros( [] , 0 ).
-countZeros( [0|Tail] , N ):-
-     countZeros( Tail , NC ),
-     N is NC + 1.
-countZeros( [Head|Tail] , N ):-
-     countZeros( Tail , NC ),
-     N is NC.
+countZeros( [] , 0 ).                % if no numbers, then 0 zeros.
+countZeros( [ 0 | Tail ] , N ):-     % if Head 0, then add to zeros.
+    countZeros( Tail , NC ),         % check recursively
+    N is NC + 1.                     % add one for each zero.
+countZeros( [ Head | Tail ] , N ):-  % if Head is not 0, then keep checking.
+    countZeros( Tail , NC ),         % check recursively
+    N is NC.                         % if no zero, then count remains the same.
