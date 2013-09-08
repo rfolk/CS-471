0d9b06d0c6b661bd16d08e54709027f3a49a5ed4
Russell Folk <rfolk@russellfolk.com>
2013-09-07 19:07:10 -0600

    Added some basic commenting to the countZeros script.

--- Assignment1/countZeros.pl
+++ Assignment1/countZeros.pl
@@ -1,7 +1,13 @@
+% Russell Folk
+% CS471: Assignment 1
+
+% Count the number of zeros in a given list and return the value N based on the
+% number counted, NC, using the recursive solution.
+
 countZeros( [] , 0 ).
-countZeros( [0|Tail] , R ):-
-     countZeros( Tail , RT ),
-     R is RT + 1.
-countZeros( [Head|Tail] , R ):-
-     countZeros( Tail , RT ),
-     R is RT.
+countZeros( [0|Tail] , N ):-
+     countZeros( Tail , NC ),
+     N is NC + 1.
+countZeros( [Head|Tail] , N ):-
+     countZeros( Tail , NC ),
+     N is NC.
