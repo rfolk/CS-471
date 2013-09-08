56c73c4d815e7e9c82ce889abe4e7923bf93be42
Russell Folk <rfolk@russellfolk.com>
2013-09-07 21:28:09 -0600

    Added test cases to each file.

--- Assignment1/countZeros.pl
+++ Assignment1/countZeros.pl
@@ -4,10 +4,17 @@
 % Count the number of zeros in a given list and return the value N based on the
 % number counted, NC, using the recursive solution.
 
-countZeros( [] , 0 ).                % if no numbers, then 0 zeros.
-countZeros( [ 0 | Tail ] , N ):-     % if Head 0, then add to zeros.
-    countZeros( Tail , NC ),         % check recursively
-    N is NC + 1.                     % add one for each zero.
-countZeros( [ Head | Tail ] , N ):-  % if Head is not 0, then keep checking.
-    countZeros( Tail , NC ),         % check recursively
-    N is NC.                         % if no zero, then count remains the same.
+countZeros( [] , 0 ).                 % if no numbers, then 0 zeros
+countZeros( [ 0 | Tail ] , N ):-      % if Head 0, then add to zeros
+    countZeros( Tail , NC ),          % check recursively
+    N is NC + 1.                      % add one for each zero
+countZeros( [ Head | Tail ] , N ):-   % if Head is not 0, then keep checking
+    countZeros( Tail , NC ),          % check recursively
+    N is NC.                          % if no zero, then count remains the same
+
+testCZ1(X):-                          % test case, should return 4
+    countZeros([0,0,0,0,1,2,3,4], X).
+testCZ2(X):-                          % test case, should return 1
+    countZeros([02,0,05,2,4,3,1], X).
+testCZ3(X):-                          % test case, should return 0
+    countZeros([02,1,05,2,4,3,1], X).
--- Assignment1/sumAll.pl
+++ Assignment1/sumAll.pl
@@ -6,3 +6,8 @@ sumAll( [] , 0 ).             % if no numbers, then N is 0.
 sumAll( [Head | Tail] , N ):- % if numbers, count recursively.
     sumAll( Tail , NC ),      % count recursively after the Head
     N is NC + Head.           % sum is current + the front of the list.
+
+testSum1(X):-                         % test case, should return 10
+    sumAll([1,2,3,4], X).
+testSum2(X):-                         % test case, should return 25
+    sumAll([5,5,5,5,5], X).
