30de77dc4c529d00245ce2b45a5b8ba26f3f59e2
Russell Folk <rfolk@russellfolk.com>
2013-09-24 19:16:59 -0600

    Made code more readable.

--- SICStus/reverse.pl
+++ SICStus/reverse.pl
@@ -4,26 +4,26 @@
 % 24 September 2013                                                           %
 %                                                                             %
 % Given a List 'source' take and reverse the elements in the list and store   %
-% in a new list 'L' then return list 'L'.                                     %
+% in a new list 'Output' then return list 'Output'.                           %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-reverse( [ ] , L ):-          % if one element in list, return one element
-    append( [ ] , [ ] , L ).
-reverse( [ H | T ] , L ):-     % if two elements, check sorted
+reverse( [ ] , Output):-          % if one element in list, return one element
+    append( [ ] , [ ] , Output).
+reverse( [ H | T ] , Output ):-   % if two elements, check sorted
     reverse( T , Rev ),
-    append( Rev , [ H ] , L ).
+    append( Rev , [ H ] , Output ).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% test case %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-testRe1(L):-                          % return [ 8 , 7 , 6 , 5 , 4 , 3 , 2 , 1 ]
-    reverse( [ 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 ] , L ).
-testRe2(L):-                          % return [ 8 , 1 , 2 , 9 , 3 ]
-    reverse( [ 3 , 9 , 2 , 1 , 8 ] , L ).
-testRe3(L):-                          % return [ 8 , 6 , 4 , 2 ]
-    reverse( [ 2 , 4 , 6 , 8 ] , L ).
-testRe4(L):-                          % return [ 2 , 1 ]
-    reverse( [ 1 , 2 ] , L ).
-testRe5(L):-                          % return [ 1 ]
-    reverse( [ 1 ] , L ).
-testRe6(L):-                          % return [ ]
-    reverse( [ ] , L ).
+testRe1(Output):-                    % return [ 8 , 7 , 6 , 5 , 4 , 3 , 2 , 1 ]
+    reverse( [ 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 ] , Output ).
+testRe2(Output):-                    % return [ 8 , 1 , 2 , 9 , 3 ]
+    reverse( [ 3 , 9 , 2 , 1 , 8 ] , Output ).
+testRe3(Output):-                    % return [ 8 , 6 , 4 , 2 ]
+    reverse( [ 2 , 4 , 6 , 8 ] , Output ).
+testRe4(Output):-                    % return [ 2 , 1 ]
+    reverse( [ 1 , 2 ] , Output ).
+testRe5(Output):-                    % return [ 1 ]
+    reverse( [ 1 ] , Output ).
+testRe6(Output):-                    % return [ ]
+    reverse( [ ] , Output ).
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% test case %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
