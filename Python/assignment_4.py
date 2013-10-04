df56434d6ce551eebde9f9e54b7f542259f03c51
Russell Folk <rfolk@russellfolk.com>
2013-10-04 15:04:43 -0600

    Fixed styling according to Python PEP.

--- Python/assignment_4.py
+++ Python/assignment_4.py
@@ -13,7 +13,7 @@ Part 4: Calculate the sum and return the list of values for the above series
 # Because I can and because it's awesome.
 import antigravity
 
-def euclideanDistance( X , Y ):
+def euclideanDistance(X, Y):
     """
     Returns the Euclidean Distance of 2 lists of numbers
     """
@@ -22,18 +22,19 @@ def euclideanDistance( X , Y ):
         raise Exception
     # start summing
     total = 0
-    for Xn , Yn in zip( X , Y ):
-        total += ( Xn - Yn ) ** 2
+    for Xn, Yn in zip(X, Y):
+        total += (Xn - Yn) ** 2
     return total ** 0.5
 
 def series(X):
     """
     Calculate the sum of 1/2^i where i = 1,n
     """
+    # if X is outside of scope, raise an exception
     if X < 1:
         raise Exception
-    total , denom = 0 , 1
-    for x in range ( 1 , X + 1 ):
+    total, denom = 0, 1
+    for x in range (1, X + 1):
         denom *= 2
         total += 1 / denom
     return total
@@ -43,11 +44,12 @@ def tuple(X):
     Calculate the sum of 1/2^i where i = 1,n
     Return the sum and the list of values
     """
+    # if X is outside of scope, raise an exception
     if X < 1:
         raise Exception
-    total , denom , result = 0 , 1 , []
-    for x in range ( 1 , X + 1 ):
+    total, denom, result = 0, 1, []
+    for x in range (1, X + 1):
         denom *= 2
         result.append(1 / denom)
         total += result[-1]
-    return (total , result)
+    return (total, result)
