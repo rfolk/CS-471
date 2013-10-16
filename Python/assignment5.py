487ef132656f336b5dcce84b1638f7dc781b61f4
Russell Folk <rfolk@russellfolk.com>
2013-10-15 21:26:26 -0600

    Parts 1 and 2 should be working.
    Haven't written unit tests, too lazy.

---
+++ Python/assignment5.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python
+
+"""CS471: Assignment 5
+Russell Folk
+15 October 2013
+
+Part 1: Calculate log2(X)
+Part 3: Calculate the sum of the series 1/2^i
+Part 4: Calculate the sum and return the list of values for the above series
+
+"""
+
+# Because I can and because it's awesome.
+# import antigravity
+
+# Part 1
+from math import log2
+# Part 2
+from math import factorial
+
+def f1(n):
+    """
+    Returns a string with the answer of log[base 2](n)
+    """
+    if isinstance(n, int) == False:
+        return "Not an integer!"
+    answer = round(log2(n), 1)
+    return "log(" + str(n) + ")=" + str(answer)
+
+def f2(n):
+    """
+    Returns a string with the answer squared of log[base 2](n)
+    """
+    if isinstance(n, int) == False:
+        return "Not an integer!"
+    answer = round(log2(n)*log2(n), 1)
+    return "log^2(" + str(n) + ")=" + str(answer)
+
+def f3(n):
+    """
+    Returns a string with the answer of n*log[base 2](n)
+    """
+    if isinstance(n, int) == False:
+        return "Not an integer!"
+    answer = round(n*log2(n), 1)
+    return "nlog(" + str(n) + ")=" + str(answer)
+
+def f4(n):
+    """
+    Returns a string with the answer of n^2
+    """
+    if isinstance(n, int) == False:
+        return "Not an integer!"
+    answer = n ** 2
+    return str(n) + "^2" + "=" + str(answer)
+
+def f5(n):
+    """
+    Returns a string with the answer of 2^n
+    """
+    if isinstance(n, int) == False:
+        return "Not an integer!"
+    answer = 2 ** n
+    return "2^" + str(n) + "=" + str(answer)
+
+def f6(n):
+    """
+    Returns a string with the answer of n!
+    """
+    if isinstance(n, int) == False:
+        return "Not an integer!"
+    answer = factorial(n)
+    return str(n) + "!=" + str(answer)
+
+def f7(n):
+    """
+    Returns a string with the answer of n^n
+    """
+    if isinstance(n, int) == False:
+        return "Not an integer!"
+    answer = n ** n
+    return str(n) + "^" + str(n) + "=" + str(answer)
+
