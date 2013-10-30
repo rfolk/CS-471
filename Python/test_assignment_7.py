7470c7826cf51d6e9715903afa815e0fc8712f4e
Russell Folk <rfolk@russellfolk.com>
2013-10-29 20:25:43 -0600

    Unit test for 3.
    Can be extended in a moment...

--- Python/test_assignment_7.py
+++ Python/test_assignment_7.py
@@ -16,9 +16,19 @@ class Test_Assignment_7(unittest.TestCase):
         case_3 = [1.5, [1.5, 4.5, 10.5, 12, 15]]
         case_4 = [1, V]
         tests = [case_1, case_2, case_3, case_4]
-        for x in tests:
-            self.assertEqual(scalar_product_bad(x[0], V), x[1])
-            self.assertEqual(scalar_product(x[0], V), x[1])
+        for t in tests:
+            self.assertEqual(scalar_product_bad(t[0], V), t[1])
+            self.assertEqual(scalar_product(t[0], V), t[1])
+
+
+    def test_vector_sum(self):
+        case_1 = [[0, 2, 4, 6], [1, 1, 1, 1], [1, 3, 5, 7]]
+        case_2 = [[0, 2, 4, 6], [2, 2, 2, 2], [2, 4, 6, 8]]
+        case_3 = [[1, 5], [0.5, 0.5], [1.5, 5.5]]
+        case_4 = [[1, 5], [0.1, 0.1], [1.1, 5.1]]
+        tests = [case_1, case_2, case_3, case_4]
+        for t in tests:
+            self.assertEqual(vector_sum(t[0], t[1]), t[2])
 
 if __name__ == '__main__' :
     unittest.main()
